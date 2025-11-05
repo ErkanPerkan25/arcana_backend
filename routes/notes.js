2a16e988b975bded6ce667422f7c780e9f05bebb
Eric <erichansson.united@gmail.com>
2025-11-05 15:43:17 -0500

    Fixing the GET call to grabb all the notes for a specific book.

--- routes/notes.js
+++ routes/notes.js
@@ -6,7 +6,22 @@ import Note from "../middleware/note_model.js";
 const router = express.Router();
 
 router.get("/", async(req, res) =>{
+    const decodedToken = validateJWT(req);
 
+    if(!decodedToken){
+        return res.status(400).json({
+            message: "Failed to verify token!",
+        });
+    }
+    else{
+        Note.find({user_id: req.body.cookie.username, book_id: req.body.book_id})
+            .then(data =>{
+                res.status(200).send(data);
+            })
+            .catch(error =>{
+                res.status(400).send(error);
+            });
+    }
 });
 
 router.post("/", async(req,res) =>{
